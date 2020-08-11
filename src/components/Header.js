dd0f80381281553bb110339a569d0d36b9098c7f
fabioluzm <fabioluzm@gmail.com>
2020-08-11 14:47:47 +0100

    Conversion from JS app to React App

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="title">Hangman</h1>
+      <p className="subtitle">Find the hidden word - Enter a letter</p>
+    </>
+  )
+}
+
+export default Header
